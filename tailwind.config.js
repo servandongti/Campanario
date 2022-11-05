28e9f89384d543319e550e4ad80bacffb436aef4
yoGhastly <ve06976@innovaccion.mx>
2022-11-04 19:42:59 -0600

    merge conlficts fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
   ],
-=======
-  content: ["./src/**/*.{html,js}"],
->>>>>>> 5e5a624fcff64cd44f7dc0dad8206741ac36ac74
   theme: {
     extend: {},
   },
