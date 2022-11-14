d396acbba4ed2328a0de02f3c349615a8c4c44a1
Servando Negrete <servandoo.12@hotmail.com>
2022-11-13 19:14:32 -0600

    links added

--- utils/socials.ts
+++ utils/socials.ts
@@ -7,17 +7,17 @@ export interface ISocial {
 export const socials: ISocial[] = [
   {
     name: 'WhatsApp',
-    url: '',
+    url: 'https://api.whatsapp.com/send?phone=5218116605609&text=Me%20interesa%20informaci%C3%B3n%20del%20Sal%C3%B3n.%0A%0AMi%20Nombre%20es%20',
     icon: '/icons/whatsapp.svg'
   },
   {
     name: 'Instagram',
-    url: '',
+    url: 'https://www.instagram.com/elcampanarioapodaca/?hl=es-la',
     icon: '/icons/instagram.svg'
   },
   {
     name: 'Facebook',
-    url: '',
+    url: 'https://www.facebook.com/saloneselcampanarioeventos/',
     icon: '/icons/facebook.svg'
   },
 ]
